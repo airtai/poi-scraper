8ab00150b304dce792c283d2331c28073e0cc5dc
Harish Mohan Raj <harish@airt.ai>
2024-11-08 11:26:45 +0000

    Add POI validator

--- poi_scraper/utils.py
+++ poi_scraper/utils.py
@@ -10,10 +10,12 @@ def is_valid_url(url):
 
 def get_all_unique_sub_urls(webpage_url: str) -> str:
     return [
-        "https://www.infofazana.hr/en/",
+        # "https://www.infofazana.hr/en/",
         # "https://www.infofazana.hr/en/what-to-see-do/outdoor-and-active-holidays/paradise-for-cyclists-and-walking/", 
         "https://www.infofazana.hr/en/what-to-see-do/outdoor-and-active-holidays/water-sports/", 
-        # "https://www.infofazana.hr/en/what-to-see-do/outdoor-and-active-holidays/for-sea-lovers/"
+        # "https://www.infofazana.hr/en/what-to-see-do/outdoor-and-active-holidays/for-sea-lovers/",
+        # "https://www.medulinriviera.info/attractions/",
+
     ]
 
 def generate_poi_markdown_table(registered_pois: dict[str, dict[str, str]]) -> str:
--- poi_scraper/workflow.py
+++ poi_scraper/workflow.py
@@ -1,7 +1,7 @@
 import os
 from typing import Annotated, Any, Optional
 
-from autogen import AssistantAgent, register_function
+from autogen import AssistantAgent, register_function, UserProxyAgent
 
 from fastagency import UI
 from fastagency.runtimes.autogen import AutoGenWorkflows
@@ -20,6 +20,38 @@ Instructions:
     - After visiting the webpage and identifying the POIs, you MUST call the `register_poi` function to record the POI.
 
 Ensure that you strictly follow these instructions to capture accurate POI data."""
+
+poi_validator_system_message = """You are a helpful agent. Your task is to determine if a given name qualifies as a Point of Interest (POI).
+
+Definition of a POI:
+    A POI is a specific place where people can visit or gather, such as tourist attractions, landmarks, parks, museums, cultural venues, and historic sites.
+    General terms that describe activities or broad categories, like "Things to do in Chennai" or "Places to visit in Chennai," are not POIs.
+
+Instructions:
+    If the given name is a POI, reply with "Yes".
+    If the given name is not a POI, reply with "No".
+    Do not provide any response other than "Yes" or "No"; you will be penalized for any additional information.
+
+Examples:
+    - name: "Marina Beach", description: "Marina Beach is a natural urban beach in Chennai, Tamil Nadu, India."
+    - Your response: "Yes"
+
+    - name: "Explore Chennai", description: "Discover the best places to visit in Chennai."
+    - Your response: "No"
+
+    - name: "Kapaleeshwarar Temple", description: "Kapaleeshwarar Temple is a Hindu temple dedicated to Lord Shiva."
+    - Your response: "Yes"
+
+    - name: "Best Restaurants in Chennai", description: "Explore the top restaurants in Chennai."
+    - Your response: "No"
+
+    - name: "Arignar Anna Zoological Park", description: "Arignar Anna Zoological Park is a zoological garden located in Vandalur, a suburb in the southwestern part of Chennai."
+    - Your response: "Yes"
+
+    - name: "Treks in Chennai", description: "Discover the best trekking spots in Chennai."
+    - Your response: "No"
+"""
+
 
 llm_config = {
     "config_list": [
@@ -42,6 +74,20 @@ def websurfer_workflow(
     registered_pois: dict[str, str] = {}
     
     summaries: list[str] = []
+
+    # Create POI agents
+    poi_validator_agent = AssistantAgent(
+        name="POI_Validator_Agent",
+        system_message=poi_validator_system_message,
+        llm_config=llm_config,
+        human_input_mode="NEVER",
+    )
+
+    poi_user_proxy = UserProxyAgent(
+        name="Poi_User_Proxy_Agent",
+        system_message="You are a helpful agent",
+        llm_config=llm_config,
+    )
     
     def register_poi(
             name: Annotated[str, "The name of POI"], 
@@ -49,26 +95,43 @@ def websurfer_workflow(
             category: Annotated[str, "The category of the POI"],
             location: Annotated[Optional[str], "The location of the POI"] = None
             ) -> str:
-        ui.text_message(sender="WebSurfer", recipient="POI Database", body=f"POI name: {name}, description: {description}, category: {category}, location: {location}")
-        registered_pois[name] = {"description": description, "category": category, "location": location}
-        return "POI registered"
+
+        def is_poi(name: str, description: str) -> str:
+            initial_message = f"Please confirm if this {name} is a Point of Interest (POI). You can use the {description} to make your decision. Reply with 'Yes' if it is a POI, and 'No' if it is not a POI."
+            chat_result = poi_user_proxy.initiate_chat(
+                poi_validator_agent,
+                message=initial_message,
+                summary_method="reflection_with_llm",
+                max_turns=1,
+            )
+            messages = [msg["content"] for msg in chat_result.chat_history]
+            last_message = messages[-1]
+            
+            return last_message
+        
+        is_valid_poi = is_poi(name, description)
+        if is_valid_poi == "Yes":
+            registered_pois[name] = {"description": description, "category": category, "location": location}
+            ui.text_message(sender="WebSurfer", recipient="POI Database", body=f"POI registered. name: {name}, description: {description}, category: {category}, location: {location}")
+            return "POI registered"
+        
+        ui.text_message(sender="WebSurfer", recipient="POI Database", body=f"POI not registered. name: {name}, description: {description}, category: {category}, location: {location}")
+        return "POI not registered"
     
+    # Get valid URL from user
     while True:
-    
         webpage_url = ui.text_input(
             sender="Workflow",
             recipient="User",
             prompt="I can collect Points of Interest (POI) data from any webpage—just share the link with me!",
         )
-
         if is_valid_url(webpage_url):
             break
-        else:
-            ui.text_message(
-                sender="Workflow",
-                recipient="User",
-                body="The provided URL is not valid. Please enter a valid URL.",
-            )
+        ui.text_message(
+            sender="Workflow",
+            recipient="User",
+            body="The provided URL is not valid. Please enter a valid URL. Example: https://www.example.com",
+        )
     
     # Collect all unique sub-links
     all_unique_sub_urls = get_all_unique_sub_urls(webpage_url)
@@ -125,9 +188,6 @@ def websurfer_workflow(
             return chat_result.summary
 
         summaries.append(scrape_poi_data(url))    
-    
-    
-    
     
     table = generate_poi_markdown_table(registered_pois)
     ui.text_message(sender="Workflow", recipient="User", body=f"List of all POIs:\n{table}")
