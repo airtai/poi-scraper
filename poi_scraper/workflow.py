812ee0137302641e260fc7e16633a242e46c8a8b
Harish Mohan Raj <harish@airt.ai>
2024-11-21 12:48:40 +0000

    Update workflow

--- poi_scraper/workflow.py
+++ poi_scraper/workflow.py
@@ -6,7 +6,7 @@ from fastagency.runtimes.autogen import AutoGenWorkflows
 
 from poi_scraper.agents import ValidatePoiAgent
 from poi_scraper.poi import PoiManager, Scraper
-from poi_scraper.utils import generate_poi_markdown_table
+from poi_scraper.utils import generate_poi_markdown_table, get_url_from_user
 
 llm_config = {
     "config_list": [
@@ -24,7 +24,7 @@ wf = AutoGenWorkflows()
 @wf.register(name="poi_scraper", description="POI scraper chat")  # type: ignore[misc]
 def websurfer_workflow(ui: UI, params: dict[str, Any]) -> str:
     # Get valid URL from user
-    base_url = "https://www.medulinriviera.info"  # get_url_from_user(ui)
+    base_url = get_url_from_user(ui)  # "https://www.medulinriviera.info"
 
     # Initialize POI manager
     poi_validator = ValidatePoiAgent(llm_config=llm_config)
